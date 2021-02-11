5d2156290bc0aa32e068924c35f21c6271e10544
Timxuv587 <1430415000@qq.com>
2021-02-12 07:34:20 +0800

    add dict

--- streamer.py
+++ streamer.py
@@ -6,7 +6,7 @@ import struct
 
 class Streamer:
     def __init__(self, dst_ip, dst_port,
-                 src_ip=INADDR_ANY, src_port=0, recv_buffer=[None*1000]):
+                 src_ip=INADDR_ANY, src_port=0, recv_buffer={}):
         """Default values listen on all network interfaces, chooses a random source port,
            and does not introduce any simulated packet loss."""
         self.socket = LossyUDP()
@@ -14,6 +14,7 @@ class Streamer:
         self.dst_ip = dst_ip
         self.dst_port = dst_port
         self.recv_buffer = recv_buffer
+        self.seq = []
 
     def send(self, data_bytes: bytes) -> None:
         """Note that data_bytes can be larger than one packet."""
@@ -27,14 +28,14 @@ class Streamer:
         # # create recv_buffer
         # recv_buffer = []
         while length - index > 1472:
-            header = "Seq: " + sequence_num + " "
+            header = "Seq: " + str(sequence_num) + " "
             data = data_bytes[index:index+1472]
-            new_data = struct.pack(header,data)
+            new_data = struct.pack(header.encode("utf-8"),data)
             self.socket.sendto(new_data, (self.dst_ip, self.dst_port))
             index += 1472
             sequence_num += 1
-
-        new_data = struct.pack(header, data_bytes[index:length])
+        header = "Seq: " + str(sequence_num) + " "
+        new_data = struct.pack(header.encode("utf-8"), data_bytes[index:length])
         self.socket.sendto(new_data, (self.dst_ip, self.dst_port))
 
         
@@ -53,6 +54,10 @@ class Streamer:
         seq_number = int(data_str.split(" ")[1])
         true_data = data_str.split(" ")[2].encode('utf-8')
         self.recv_buffer[seq_number] = true_data
+        self.seq.append(seq_number)
+        data = []
+        for i in self.seq.sort():
+            data.append(recv_buffer)
         # For now, I'll just pass the full UDP payload to the app
         if None not in self.recv_buffer:
             return None

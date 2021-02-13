92501298ae2940f83c118856d677e2330ff1c964
Timxuv587 <1430415000@qq.com>
2021-02-13 21:05:45 +0800

    part 2 finish

--- streamer.py
+++ streamer.py
@@ -15,6 +15,8 @@ class Streamer:
         self.dst_port = dst_port
         self.recv_buffer = recv_buffer
         self.seq = []
+        self.current_recv_seq = 0
+        self.current_send_seq = 0
 
     def send(self, data_bytes: bytes) -> None:
         """Note that data_bytes can be larger than one packet."""
@@ -22,20 +24,26 @@ class Streamer:
 
         # for now I'm just sending the raw application-level data in one UDP payload
         length = len(data_bytes)
-
         index = 0
-        sequence_num = 0
-        # # create recv_buffer
-        # recv_buffer = []
-        while length - index > 1472:
-            header = "Seq: " + str(sequence_num) + " "
-            data = data_bytes[index:index+1472]
-            new_data = struct.pack(header.encode("utf-8"),data)
+        #Create a header with current sequence
+        #header name and header value is seperated by ":"
+        #All headers and the true data content is seperate by ";"
+        header = "Seq:" + str(self.current_send_seq) + ";"
+        #Get the header length in order to accomodate packet size
+        l = len(header.encode())
+        while length - index > 1472 - len(header.encode()):
+            data = data_bytes[index:index+1472-l]
+            #Decode data to string, combine with header, and encode to bytes
+            new_data = (header+data.decode()).encode()
             self.socket.sendto(new_data, (self.dst_ip, self.dst_port))
-            index += 1472
-            sequence_num += 1
-        header = "Seq: " + str(sequence_num) + " "
-        new_data = struct.pack(header.encode("utf-8"), data_bytes[index:length])
+            index += 1472 - l
+            #Update seq num and header
+            self.current_send_seq += 1
+            header = "Seq:" + str(self.current_send_seq) + ";"
+            l = len(header.encode())
+        header = "Seq:" + str(self.current_send_seq) + ";"
+        new_data = (header + data_bytes[index:length].decode()).encode()
+        self.current_send_seq += 1
         self.socket.sendto(new_data, (self.dst_ip, self.dst_port))
 
         
@@ -43,25 +51,25 @@ class Streamer:
     def recv(self) -> bytes:
         """Blocks (waits) if no data is ready to be read from the connection."""
         # your code goes here!  The code below should be changed!
-        
-        # this sample code just calls the recvfrom method on the LossySocket
-        data, addr = self.socket.recvfrom()
-        # TODO: needs to solve
-        # if len(self.recv_buffer) < len(data):
-        #     self.recv_buffer = [None*len(data)]
 
-        data_str = struct.unpack(data).decode('utf-8')
-        seq_number = int(data_str.split(" ")[1])
-        true_data = data_str.split(" ")[2].encode('utf-8')
-        self.recv_buffer[seq_number] = true_data
-        self.seq.append(seq_number)
-        data = []
-        for i in self.seq.sort():
-            data.append(recv_buffer)
-        # For now, I'll just pass the full UDP payload to the app
-        if None not in self.recv_buffer:
-            return None
-        return data
+        #If data out of order, keep receiving till the packet of current sequence number is received. 
+        while(self.current_recv_seq not in self.seq):
+            # this sample code just calls the recvfrom method on the LossySocket
+            data, addr = self.socket.recvfrom()
+
+            #header name and header value is seperated by ":"
+            #All headers and the true data content is seperate by ";"
+            data_str = data.decode('utf-8')
+            seq_number = int(data_str.split(";")[0].split(":")[1])
+            true_data = data_str.split(";")[1].encode()
+
+            #Store data to buffer base on sequence number
+            self.recv_buffer[seq_number] = true_data
+            self.seq.append(seq_number)
+
+        #Return data that match current seq number
+        self.current_recv_seq += 1
+        return  self.recv_buffer[self.current_recv_seq-1]
 
     def close(self) -> None:
         """Cleans up. It should block (wait) until the Streamer is done with all
